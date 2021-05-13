e2e6fea09452ee194096ed87ea9d7b10d7a6c1bf
Szymon Uscinski <szymonuscinski@gmail.com>
2021-05-13 14:38:45 +0200

    added functions that render view and add new tasks

---
+++ js/script.js
@@ -0,0 +1,44 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent
+        });
+
+        render();
+    }
+
+    const render = () => {
+        let listOfTasks = "";
+
+        for (task of tasks) {
+            listOfTasks += `
+            <li>
+            ${task.content}
+            </li>`
+        }
+
+        document.querySelector(".js-tasks").innerHTML = listOfTasks;
+    }
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            addNewTask(newTaskContent);
+        });
+    }
+
+    init();
+}
